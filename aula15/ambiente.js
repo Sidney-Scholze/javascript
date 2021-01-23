22a80d74eab64025e590aee000ca621f8f317a97
Sidney Scholze <sidney.scholze@gmail.com>
2021-01-23 16:34:39 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,7 +1,7 @@
 let num = [5, 8, 9, 3, 7, 1]
 console.log(`Nosso vetor é ${num}`)
 //num.sort()
-//num[4] = 7 // Adiciona na posição 5
+num[6] = 2 // Adiciona na posição 6 começando da posição 0
 //num.push(30) // Adiciona na última posição
 /*
 for(let i = 0; i < num.length; i++){
